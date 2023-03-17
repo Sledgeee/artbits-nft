c522e97474636363d703da6970e7d05e31c66c94
Sledgeee <bazisc.qa@gmail.com>
2023-03-17 11:36:51 +0200

    remove await from app init

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,18 +1,18 @@
 import "../css/app.css";
 import "./bootstrap";
 
-import {createInertiaApp} from "@inertiajs/react";
-import {resolvePageComponent} from "laravel-vite-plugin/inertia-helpers";
-import {createRoot} from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createRoot } from "react-dom/client";
 
-await createInertiaApp({
+createInertiaApp({
     title: (title) => `${title} - ArtBits`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.tsx`,
             import.meta.glob("./Pages/**/*.tsx")
         ),
-    setup({el, App, props}) {
+    setup({ el, App, props }) {
         const root = createRoot(el);
 
         root.render(<App {...props} />);
