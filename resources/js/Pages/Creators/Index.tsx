01d3c2676df2b661e9150928702e483306528592
Sledgeee <bazisc.qa@gmail.com>
2023-03-17 13:27:14 +0200

    fix names and add link to register page from login page

--- resources/js/Pages/Creators/Index.tsx
+++ resources/js/Pages/Creators/Index.tsx
@@ -1,34 +1,29 @@
-import {FC} from 'react'
-import {Head} from "@inertiajs/react";
+import CreatorPageCard from "@/Components/creators/creatorPage/CreatorPageCard";
 import Layout from "@/Components/Layout";
-import HeaderImage from "@/Components/nft/nft_page/HeaderImage";
 import DefaultNftList from "@/Components/nft/DefaultNftList";
-import {Nft} from "@/types/nft.type";
-import {BaseProps} from "@/types/base.type";
-import CreatorPageCard from "@/Components/creators/creator_page/CreatorPageCard";
-import {User} from "@/types/user.type";
+import HeaderImage from "@/Components/nft/nftPage/HeaderImage";
+import { BaseProps } from "@/types/base.type";
+import { Nft } from "@/types/nft.type";
+import { User } from "@/types/user.type";
+import { Head } from "@inertiajs/react";
+import { FC } from "react";
 
 interface CreatorProps extends BaseProps {
-    user: User
-    creatorItems: Nft[]
+    user: User;
+    creatorItems: Nft[];
 }
 
-const Index: FC<
-    CreatorProps
-> = ({
-         creatorItems,
-         auth,
-         user
-     }) => {
-    return <>
-        <Head title={user?.username}/>
-        <Layout auth={auth}>
-            <HeaderImage image={user.creator.banner_image || ''}/>
-            <CreatorPageCard user={user}/>
-            <DefaultNftList trendingNftList={creatorItems}/>
-        </Layout>
-    </>
+const Index: FC<CreatorProps> = ({ creatorItems, auth, user }) => {
+    return (
+        <>
+            <Head title={user?.username} />
+            <Layout auth={auth}>
+                <HeaderImage image={user.creator.banner_image || ""} />
+                <CreatorPageCard user={user} />
+                <DefaultNftList trendingNftList={creatorItems} />
+            </Layout>
+        </>
+    );
+};
 
-}
-
-export default Index
+export default Index;
--- resources/js/Pages/CurrentNft.tsx
+++ resources/js/Pages/CurrentNft.tsx
@@ -1,39 +1,33 @@
-import {FC} from 'react'
-import HeaderImage from "@/Components/nft/nft_page/HeaderImage";
-import {Head} from "@inertiajs/react";
 import Layout from "@/Components/Layout";
-import {BaseProps} from "@/types/base.type";
-import {Nft} from "@/types/nft.type";
-import NftPageCard from "@/Components/nft/nft_page/NftPageCard";
 import DefaultNftList from "@/Components/nft/DefaultNftList";
+import HeaderImage from "@/Components/nft/nftPage/HeaderImage";
+import NftPageCard from "@/Components/nft/nftPage/NftPageCard";
+import { BaseProps } from "@/types/base.type";
+import { Nft } from "@/types/nft.type";
+import { Head } from "@inertiajs/react";
+import { FC } from "react";
 
 interface CurrentNftProps extends BaseProps {
-    nft: Nft,
-    nfts: Nft[]
+    nft: Nft;
+    nfts: Nft[];
 }
 
-const CurrentNft: FC<
-    CurrentNftProps
-> = ({
-         auth,
-         nft,
-         nfts
-     }) => {
-    return <>
-        <Head title={nft.name}/>
-        <Layout auth={auth}>
-            <HeaderImage image={nft.header_image}/>
-            <NftPageCard nft={nft}/>
-            <DefaultNftList
-                trendingNftList={nfts}
-                title='More From This Artist'
-                buttonName='Go to artist page'
-                buttonHref={'/creator/' + nft.creator.user?.username}
-            />
-        </Layout>
-    </>
+const CurrentNft: FC<CurrentNftProps> = ({ auth, nft, nfts }) => {
+    return (
+        <>
+            <Head title={nft.name} />
+            <Layout auth={auth}>
+                <HeaderImage image={nft.header_image} />
+                <NftPageCard nft={nft} />
+                <DefaultNftList
+                    trendingNftList={nfts}
+                    title="More From This Artist"
+                    buttonName="Go to artist page"
+                    buttonHref={"/creator/" + nft.creator.user?.username}
+                />
+            </Layout>
+        </>
+    );
+};
 
-
-}
-
-export default CurrentNft
+export default CurrentNft;
