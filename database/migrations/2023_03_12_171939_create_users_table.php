b31b992927861ba06e2ebdb09c790b6e72a30fbd
Oleh Voloshyn <76629564+Sledgeee@users.noreply.github.com>
2023-06-04 19:49:58 +0300

    Update 2023_03_12_171939_create_users_table.php

--- database/migrations/2023_03_12_171939_create_users_table.php
+++ database/migrations/2023_03_12_171939_create_users_table.php
@@ -17,10 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
-            $table->string('bio', 256)->nullable();
-            $table->string('banner_image', 128)->nullable();
+            $table->string('bio', 1024)->nullable();
+            $table->string('banner_image', 1024)->nullable();
             $table->string('metamask_address', 42)->nullable();
-            $table->string('avatar_image', 128)->nullable();
+            $table->string('avatar_image', 1024)->nullable();
             $table->timestamps();
         });
     }
